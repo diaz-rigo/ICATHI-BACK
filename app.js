3e3b9d46644cb4e585f5fccabd6c14d6e6a3bde0
Antonius <20221136@uthh.edu.mx>
2024-12-11 21:33:28 -0600

    `Added and modified routes and controllers for aspirante registration`

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ const plantelesRouter = require ('./routes/planteles_cursosRoutes')
 const correoRoutes = require ('./routes/correoRouter')
 const postulacion = require ('./routes/postulacionRouter')
 const alumno = require ('./routes/alumnoRouter')
-const aspirante= require ('./routes/registrosAspirantesRouter')
 
+const aspirante= require ('./routes/registrosAspirantesRouter')
 
 // const pool = require('./config/database'); // Importa el pool de conexión
 
@@ -55,8 +55,8 @@ app.use('/enviar-correo', correoRoutes);
 app.use('/postulacion', postulacion);
 app.use('/planteles', plantelesRouter);
 app.use('/alumno', alumno);
-app.use('/aspirante', aspirante);
 
+app.use('/aspirante', aspirante);
 // Middleware para manejar errores de rutas no encontradas
 app.use((req, res, next) => {
   const error = new Error('Recurso no encontrado');
