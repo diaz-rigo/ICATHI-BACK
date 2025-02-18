8470c0ace1b4be95db6cad4500e747ed706706ce
diaz-rigo <morelosalfaro@gmail.com>
2025-02-17 21:14:30 -0600

    cambio de conexion string

--- config/database.js
+++ config/database.js
@@ -1,21 +1,14 @@
 const { Pool } = require('pg');
 
 // // Configuración del pool de conexiones
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL || 'postgresql://icathi:Cz6YjxCSPnPVDXWxfbOE4XBqRv71StU6@dpg-ct4eqi5umphs73e5ohi0-a.oregon-postgres.render.com/icathi',
-//   ssl: {
-//     rejectUnauthorized: false, // Para conexiones SSL a servicios en la nube como Render
-//   },
-// });
 const pool = new Pool({
-  user: process.env.DB_USER || "postgres",  
-  host: process.env.DB_HOST || "201.116.27.119", // Dirección del servidor
-  database: process.env.DB_NAME || "icathi", // Nombre de la BD
-  password: process.env.DB_PASSWORD || "postgres", // Contraseña
-  port: process.env.DB_PORT || 5432, // Puerto de PostgreSQL (por defecto es 5432)
-  ssl: false,  // Asegúrate de que sea false si no usas SSL
-
+  connectionString: process.env.DATABASE_URL || 'postgresql://iicathi:IukFwhv3roNxG7uwUMgPQawmWNEaBpIc@dpg-cupv77lumphs73ebghs0-a.oregon-postgres.render.com/iicathi',
+  ssl: {
+    rejectUnauthorized: false, // Para conexiones SSL a servicios en la nube como Render
+  },
 });
+
+
 // Verificar conexión al inicializar
 pool.connect()
   .then(client => {
