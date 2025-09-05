c8b3af67d6e170393f1b86a6e1a56e44a180c41b
diaz-rigo <morelosalfaro@gmail.com>
2025-09-05 12:44:34 -0600

    sxa

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ app.use('/solicitudes-cursos', solicitudesCursosRoutes);
 // Middleware para manejar errores de rutas no encontradas
 app.use((req, res, next) => {
     const error = new Error('Recurso no encontrado');
-    error.status = 404;
+    error.status = 404; 
     next(error);
 });
 
